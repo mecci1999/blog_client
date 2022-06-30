069c0cf77b37481c1b73dc454e78e464db6edcf6
cc <664751829@qq.com>
2022-07-01 00:25:32 +0800

    feat: 增加实时更改壁纸方法

--- src/view/about/index.vue
+++ src/view/about/index.vue
@@ -1,12 +1,46 @@
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { log } from 'console';
+import {
+  defineComponent,
+  onUnmounted,
+  onMounted,
+  computed,
+  ref,
+  reactive,
+} from 'vue';
+import {
+  getCurrnetTime,
+  changeBackgroundImageByTime,
+} from '../../utils/changeBackgroundImage';
 
 export default defineComponent({
   name: 'AppAbout',
 
   setup() {
+    let time = ref('');
+    let date: any;
+    let style = reactive({
+      fontSize: 14 + 'px',
+      backgroundImage: '',
+    });
 
-    return {}
+    onMounted(() => {
+      date = setInterval(() => {
+        time.value = getCurrnetTime();
+        style.backgroundImage = changeBackgroundImageByTime(
+          time.value.slice(0, 2),
+        );
+      }, 1000);
+    });
+
+    onUnmounted(() => {
+      clearInterval(date);
+    });
+
+    return {
+      time,
+      style,
+    };
   },
 
   components: {},
@@ -14,7 +48,7 @@ export default defineComponent({
 </script>
 
 <template>
-  <div class="app-about">AppAbout</div>
+  <div class="app-about" :style="style"></div>
 </template>
 
 <style lang="scss" scoped>
