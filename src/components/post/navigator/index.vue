bc60c673ef027ec9e416ca59c1ce3030982c2f38
cc <664751829@qq.com>
2022-07-12 22:18:19 +0800

    内容页面导航组件100%

--- src/components/post/navigator/index.vue
+++ src/components/post/navigator/index.vue
@@ -1,8 +1,10 @@
 <script lang="ts" setup>
 import { computed, reactive } from 'vue';
+import { useRouter } from 'vue-router';
 import { useStore } from 'vuex';
 
 const store = useStore();
+const router = useRouter();
 
 const currentIsFirstPost = computed(
   () => store.getters['post/currentIsFirstPost'],
@@ -16,12 +18,19 @@ const getBackPost = computed(() => store.getters['post/getBackPost']);
 
 const onClickForwardJump = () => {
   // 跳转到前一篇博客
+  router.replace({
+    name: 'postShow',
+    params: { postId: getForwardPost.value.id },
+  });
 };
 
 const onClickBackJump = () => {
   // 跳转到后一篇博客
+  router.replace({
+    name: 'postShow',
+    params: { postId: getBackPost.value.id },
+  });
 };
-
 </script>
 
 <template>
@@ -31,13 +40,13 @@ const onClickBackJump = () => {
       v-if="!currentIsFirstPost"
       @click="onClickForwardJump"
     >
-    <img class="post-navigator-forward-img" src="@/assets/image/git.png">
+      <img class="post-navigator-forward-img" src="@/assets/image/git.png" />
+      <div class="post-navigator-forward-shadow"></div>
       <div class="post-navigator-forward-text">
-        
-        <span class="post-navigator-forward-text-top">上一篇</span>
-        <span class="post-navigator-forward-text-bottom">{{
-          getForwardPost?.title
-        }}</span>
+        <div class="post-navigator-forward-text-top">上一篇</div>
+        <div class="post-navigator-forward-text-bottom">
+          {{ getForwardPost?.title }}
+        </div>
       </div>
     </div>
     <div
@@ -45,7 +54,11 @@ const onClickBackJump = () => {
       v-if="!currentIsEndPost"
       @click="onClickBackJump"
     >
-    <img class="post-navigator-back-img" src="@/assets/image/r_statistics.jpeg">
+      <img
+        class="post-navigator-back-img"
+        src="@/assets/image/r_statistics.jpeg"
+      />
+      <div class="post-navigator-back-shadow"></div>
       <div class="post-navigator-back-text">
         <span class="post-navigator-back-text-top">下一篇</span>
         <span class="post-navigator-back-text-bottom">{{
